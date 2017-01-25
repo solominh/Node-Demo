f6134effec9cf4f056e8157c02ebec3f006d9100
solominh <hoangminhftu2@gmail.com>
2017-01-26 06:02:23 +0700

    [Lesson 4] Middlewares handle errors

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require('express')  
+const express = require('express')
 const app = express()
 
-app.use((request, response, next) => {  
-  console.log(request.headers)
-  next()
+app.get('/', (request, response) => {
+  throw new Error('oops')
 })
 
-app.use((request, response, next) => {  
-  request.chance = Math.random()
-  next()
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
 })
 
-app.get('/', (request, response) => {  
-  response.json({
-    chance: request.chance
-  })
-})
-
-app.listen(3000)  
+app.listen(3000)
