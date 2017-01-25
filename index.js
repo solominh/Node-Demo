6c52d0cf3b72b1b086438fd6b8e79fbe2e03cac9
solominh <hoangminhftu2@gmail.com>
2017-01-26 06:25:39 +0700

    [Lession 4] Use Handlebars template engine

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
+const path = require('path')  
+const express = require('express')  
+const exphbs = require('express-handlebars')
+
 const app = express()
 
-app.get('/', (request, response) => {
-  throw new Error('oops')
-})
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')  
+app.set('views', path.join(__dirname, 'views'))  
 
-app.use((err, request, response, next) => {
-  // log the error, for now just console.log
-  console.log(err)
-  response.status(500).send('Something broke!')
+
+app.get('/', (request, response) => {  
+  response.render('home', {
+    name: 'John'
+  })
 })
 
 app.listen(3000)
