3060176450feafcdaefe730a76c901d405b5ee31
solominh <hoangminhftu2@gmail.com>
2017-01-26 05:46:07 +0700

    [Lession 4] Add first example

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require('./app/index') 
+const http = require('http')  
+const port = 3000 // http://localhost:3000/
 
+const requestHandler = (request, response) => {  
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
 
-console.log('hello from Node.js')  
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
