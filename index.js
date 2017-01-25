ef1eb797218c718089516476967d72350fe9fa3c
solominh <hoangminhftu2@gmail.com>
2017-01-26 05:57:38 +0700

    [Lesson 4] Use middlewares

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')  
-const app = express()  
-const port = 3000
+const app = express()
+
+app.use((request, response, next) => {  
+  console.log(request.headers)
+  next()
+})
+
+app.use((request, response, next) => {  
+  request.chance = Math.random()
+  next()
+})
 
 app.get('/', (request, response) => {  
-  response.send('Hello from Express!')
+  response.json({
+    chance: request.chance
+  })
 })
 
-app.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.listen(3000)  
